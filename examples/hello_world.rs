517af1800e57e880cf18da2c7b8a9e0ee4e2fe18
Nolan Darilek <nolan@thewordnerd.info>
2020-12-16 03:26:03 -0600

    Fix examples.

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -5,10 +5,10 @@ fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(bevy_tts::TtsPlugin)
-        .add_system(bevy::input::system::exit_on_esc_system)
-        .add_startup_system(setup)
-        .add_system(event_poll)
-        .add_system(greet)
+        .add_system(bevy::input::system::exit_on_esc_system.system())
+        .add_startup_system(setup.system())
+        .add_system(event_poll.system())
+        .add_system(greet.system())
         .run();
 }
 
