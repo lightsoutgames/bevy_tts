2bf7ce232019f0c800071bf0093928e6f8ca4194
Nolan Darilek <nolan@thewordnerd.info>
2022-01-21 13:13:15 -0600

    Remove `Clone` since it isn't support on macOS.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use crossbeam_channel::{unbounded, Receiver};
 pub use tts::{Backends, Error, Features, Tts, UtteranceId};
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub enum TtsEvent {
     UtteranceBegin(UtteranceId),
     UtteranceEnd(UtteranceId),
