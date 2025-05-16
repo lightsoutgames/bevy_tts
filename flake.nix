b3be60ea8d8da901a101ed3b59d5d8e704653da5
Nolan Darilek <nolan@thewordnerd.info>
2025-05-16 09:22:33 -0500

    chore: Add Nix-based build and CI configuration.

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk/master";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      naersk,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell.override { stdenv = pkgs.clangStdenv; } rec {
+            nativeBuildInputs = [
+              cargo
+              rustc
+              rustfmt
+              rustPackages.clippy
+              pkg-config
+              pre-commit
+              git-cliff
+            ];
+            buildInputs = [
+              speechd
+              udev
+              alsa-lib
+              vulkan-loader
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+              libxkbcommon
+              wayland
+            ];
+            shellHook = ''
+              export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
+              export RUSTFLAGS="-C link-arg=-Wl,-rpath,${lib.makeLibraryPath buildInputs}"
+              pre-commit install
+            '';
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      }
+    );
+}
